24438bbcbe2300d5b89669fa5135e6dc82445359
Nikhil Shahi <shniks@gmail.com>
2022-08-14 13:35:35 -0700

    Created test to generate the offset

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -13,10 +13,16 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  def test_it_can_create_a_5_digit_key
+  def test_it_can_create_a_5_digit_random_number
     shift = Shift.new
 
     assert_equal 5, shift.random.length
+  end
+
+  def test_it_can_generate_the_offset
+    shift = Shift.new
+
+    assert_equal 4, shift.offset.length
   end
 
 end
