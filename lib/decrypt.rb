9e332d90270d7dfa3a703d74c107f134d46caba1
Nikhil Shahi <shniks@gmail.com>
2022-08-15 08:55:55 -0700

    Created decrypt runner file.

---
+++ lib/decrypt.rb
@@ -0,0 +1,17 @@
+require './lib/enigma'
+require 'pry'
+
+#Read command line
+filename = ARGV.first
+output = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+
+# Parse the message to be decrypted
+cipher = File.read(filename).gsub("\n", "")
+
+# Return the encrypted message, key used and date used
+enigma =  Enigma.new.decrypt(cipher, key, date)
+# Write the encrypted message to a new file and print confirmation in Terminal
+File.write("#{output}", enigma[:decryption])
+puts "Created '#{output}' with the key #{enigma[:key]} and date #{enigma[:date]}"
