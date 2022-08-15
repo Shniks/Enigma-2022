1a6543b9de7236fb0c7ec998d5ad89169b12938c
Nikhil Shahi <shniks@gmail.com>
2022-08-14 17:01:52 -0700

    Created test and initialized a method for the alphabetical dictionary.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,15 @@
+require './lib/encryptor'
+
 class Enigma
 
-  def encrypt(message, key = nil, date = nil)
+  attr_reader :dict
 
+  def initialize
+    @dict = ("a".."z").to_a << " "
   end
 
-
-
+  def encrypt(message, key = nil, date = nil)
+    Encryptor.new(message, key, date)
+  end
 
 end
